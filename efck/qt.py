c1925eb70df2862f5acdba81190ed1702d34cd96
Kernc <kerncece@gmail.com>
2022-09-02 18:04:58 +0200

    REF: Log qt import errors

--- efck/qt.py
+++ efck/qt.py
@@ -1,28 +1,41 @@
+"""
+A single module containing all used classes from Qt.
+"""
+import logging as _logging
 import os as _os
+import typing as _typing
 from importlib import import_module as _import_module
 
-QT_API = _os.environ.get('QT_API')  # QtPy honors this variable, so we can too
-if QT_API in ('pyqt6', 'pyside6', 'pyqt5'):
-    _mod = _import_module(f'.{QT_API}', __package__ + '._qt')
-    globals().update(_mod.__dict__)
-else:
+# Specify literally for IDE typing/autocompletion
+if _typing.TYPE_CHECKING:
     try:
         from ._qt.pyqt6 import *
-        QT_API = 'pyqt6'
     except ImportError:
         try:
             from ._qt.pyside6 import *
-            QT_API = 'pyside6'
         except ImportError:
             try:
                 from ._qt.pyqt5 import *
-                QT_API = 'pyqt5'
             except ImportError:
-                try:
-                    from ._qt.qtpy import *
-                    from qtpy import API_NAME as QT_API
-                except ImportError:
-                    pass
+                pass
+
+_logger = _logging.getLogger(__name__)
+
+QT_API = _os.environ.get('QT_API', '').lower()  # QtPy uses this variable, so we can too
+if QT_API:
+    _logger.info('Obeying env variable QT_API="%s"', QT_API)
+
+try_apis = (QT_API,) if QT_API else ('pyqt6', 'pyside6', 'pyqt5')
+for qt_api in try_apis:
+    try:
+        _mod = _import_module(f'.{qt_api}', __package__ + '._qt')
+    except ImportError as exc:
+        _logger.warning('Failed to import %s: %s: %s', qt_api, exc.__class__.__name__, exc)
+    else:
+        globals().update(_mod.__dict__)
+        QT_API = qt_api
+        break
+
 try:
     QApplication
 except NameError:
