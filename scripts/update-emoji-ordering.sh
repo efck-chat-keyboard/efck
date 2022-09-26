bfbd6d8adf4b4c0ed946c8e89de7dd531adf6d6c
Kernc <kerncece@gmail.com>
2022-09-26 08:31:14 +0200

    MNT: Update emoji-ordering.txt to Unicode 15

--- scripts/update-emoji-ordering.sh
+++ scripts/update-emoji-ordering.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eux
 
-UNICODE_VERSION=14.0
+UNICODE_VERSION=15.0  # FIXME: Figure out how to bump this automatically
 
 cd "$(dirname $0)/../efck"
 curl -O "https://unicode.org/emoji/charts-${UNICODE_VERSION}/emoji-ordering.txt"
